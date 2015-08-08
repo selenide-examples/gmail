f67bd9d051e821790e4bbaca8f8a6e42599931a5
Djamshed Melikov <djamshed.melikov@nytimes.com>
2015-08-08 01:07:12 -0400

    Add PhantomJS Driver to run in background

--- test/com/selenide/sugar/util/TestCase.java
+++ test/com/selenide/sugar/util/TestCase.java
@@ -30,7 +30,7 @@ public abstract class TestCase {
   }
 
   protected static void waitUntilPagesIsLoaded() {
-    $(byText("Loading")).waitUntil(disappears, 20000);
+    $(byText("Loading")).waitUntil(disappears, 2000000);
   }
 
   @AfterClass
@@ -39,9 +39,13 @@ public abstract class TestCase {
   }
 
   private static void login() {
-    $("#user_name").val(System.getProperty("sugar.username", "dummy"));
-    $("#user_password").val(System.getProperty("sugar.password", "dummy"));
-    $("#login_button").click();
+    //$("#user_name").val(System.getProperty("sugar.username", "dummy"));
+    //$("#user_password").val(System.getProperty("sugar.password", "dummy"));
+    //$("#login_button").click();
+    $("#Email").val(System.getProperty("gmail.username", "dummy"));
+    $("#next").click();
+    $("#Passwd").val(System.getProperty("gmail.password", "dummy"));
+    $("#signIn").click();
     $(".error-msg").waitUntil(disappears, 2000);
   }
 }
