0036939d18c434e7178142e025f69079aff4b49a
Andrei Solntsev <andrei.solntsev@gmail.com>
2023-10-25 23:42:12 +0300

    upgrade to Selenide 7.0.0 and Java 17

--- test/org/selenide/examples/gmail/Highlighter.java
+++ test/org/selenide/examples/gmail/Highlighter.java
@@ -1,31 +1,24 @@
 package org.selenide.examples.gmail;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
-
 import static com.codeborne.selenide.Selenide.executeJavaScript;
 import static com.codeborne.selenide.Selenide.sleep;
 
-public class Highlighter extends AbstractWebDriverEventListener {
-  @Override
-  public void beforeClickOn(WebElement element, WebDriver driver) {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.WebDriverListener;
+
+public class Highlighter implements WebDriverListener {
+
+  @Override public void beforeClick(WebElement element) {
     highlight(element, "orange");
   }
 
-  @Override
-  public void beforeFindBy(By by, WebElement element, WebDriver driver) {
-    highlight(element, "red");
-  }
-
-  @Override
-  public void afterFindBy(By by, WebElement element, WebDriver driver) {
+  @Override public void afterFindElement(WebDriver driver, By locator, WebElement element) {
     highlight(element, "green");
   }
 
-  @Override
-  public void beforeChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
+  @Override public void beforeSendKeys(WebElement element, CharSequence... keysToSend) {
     highlight(element, "orange");
   }
 
@@ -37,11 +30,11 @@ public class Highlighter extends AbstractWebDriverEventListener {
     if (element != null && element.getAttribute("__selenideHighlighting") == null) {
       for (int i = 1; i < 4; i++) {
         transform(element, color, i);
-        sleep(50);
+        sleep(150);
       }
       for (int i = 4; i > 0; i--) {
         transform(element, color, i);
-        sleep(50);
+        sleep(150);
       }
     }
     return element;
